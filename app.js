c23ec0b088b4ae4798df6e3b13241cde683a19f3
levvw <abdelraahmanmostafa@gmail.com>
2021-10-15 22:19:59 +0200

    request fetching api

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-console.log("start");
-setTimeout(() => {
-  console.log("Printed after 3 secs");
-}, 3000);
-console.log("end");
+const request = require("request"); // npm i request
+// const URL = "api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appidc5395899415e6f2bd6a1ec022bddde98";
+const URL = "https://swapi.dev/api/people/1/";
+request({ url: URL }, (error, data) => {
+  console.log(data);
+});
