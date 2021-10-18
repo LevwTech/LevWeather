fa0a9e2c91b72eee70594f6c6ab62a51f21a8fd4
levvw <abdelraahmanmostafa@gmail.com>
2021-10-18 21:09:50 +0200

    added error handeling for fetching apis

---
+++ 6.1 n3-06-06-geocode.zip/n3-06-06-geocode/weather-app/app.js
@@ -0,0 +1,18 @@
+const request = require('request')
+
+const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233'
+
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+})
+
+// Geocoding
+// Address -> Lat/Long -> Weather
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+    const latitude = response.body.features[0].center[0]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
+})
--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const request = require("request"); // npm i request
-// my weather api key: c5395899415e6f2bd6a1ec022bddde98
-const URL =
+const request = require("request");
+const url =
   "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=c5395899415e6f2bd6a1ec022bddde98";
-request({ url: URL }, (error, data) => {
-  // console.log(JSON.parse(data.body));
-  // console.log(data);
-  console.log(`it is currently ${JSON.parse(data.body).main.temp} degrees`);
+const geocodeURL =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibGV2dyIsImEiOiJja3V3dnh0ZXcwbzlpMnByZnZvMGZ4Y2xzIn0.JI7ULRIutDs35rWlEIbuMA";
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  if (error) {
+    console.log("something went wrong");
+  } else {
+    if (response.body.message === "Not Authorized - Invalid Token") {
+      console.log("something went wrong");
+      return;
+    }
+    const latitude = response.body.features[0].center[0];
+    const longitude = response.body.features[0].center[1];
+    console.log(latitude, longitude);
+  }
 });
+
+request(
+  {
+    url: url,
+  },
+  (error, data) => {
+    if (error) {
+      console.log("something went wrong");
+    } else {
+      if (JSON.parse(data.body).cod === 401) {
+        console.log("something went wrong");
+        return;
+      }
+      console.log(`it is currently ${JSON.parse(data.body).main.temp} celsius`);
+    }
+  }
+);
